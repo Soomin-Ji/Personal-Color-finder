c87aa772c48b1ce94efa39ca18cab987aeddb018
Soomin <113295627+Soomin-Ji@users.noreply.github.com>
2022-12-11 20:57:27 +0900

    Delete importing_image.py

--- importing_image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-img = cv2.imread('zd.jpeg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img)
-plt.show()
