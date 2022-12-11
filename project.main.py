db5a712d0335dc68f3c94bcadc032ba954f6576e
Soomin <113295627+Soomin-Ji@users.noreply.github.com>
2022-12-11 21:09:16 +0900

    Update project.main.py

--- project.main.py
+++ project.main.py
@@ -4,6 +4,13 @@ import numpy as np
 cat_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
+while(False):
+    print("Webcam is not running")
+    exit()
+
+time_num = 0
+image_num = 0
+
 while (True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -12,8 +19,18 @@ while (True):
     for (x,y,w,h) in faces:
         print(x,y,w,h)
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-       
-    cv2.imshow('img', img)
+    
+    status, frame = cap.read()
+    time_num = time_num + 1
+
+    cv2.imshow('webcam', img)
+
+    if time_num == 30:
+        image_num = image_num + 1
+
+    cv2.imwrite('img'+str(image_num)+'.png', frame) #-- 본인 편의에 맞게 경로 설정 및 이미지 이름 변경
+    time_num = 0
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
