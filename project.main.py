488225a7befbb44a95aa024c7b2aa39dcea08bf5
jared030923 <67725001+jared030923@users.noreply.github.com>
2022-12-13 13:30:20 +0900

    메인 파일 수정
    완성본

--- project.main.py
+++ project.main.py
@@ -3,6 +3,7 @@ import numpy as np
 
 cat_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
+#img = cv2.imread("./photo.jpg")
 
 spring_warm = cv2.imread('./spring_warm.jpg')
 summer_cool = cv2.imread('./summer_cool.jpg')
@@ -29,10 +30,45 @@ while (True):
         print("captured")
         cv2.imwrite("capture.jpg", img)
         break
+    #cv2.imwrite("capture.jpg", img)
+    #break
 
-src = cv2.imread("capture.jpg", cv2.IMREAD_COLOR)
-dst = src[x:x+w, y:y+h].copy() #얼굴만 잘라낸 사진 
+    #break
 
-cv2.imshow("dst", dst)
-cap.release()
-cv2.distroyAllWindows()
+src = cv2.imread("./capture.jpg", cv2.IMREAD_COLOR)
+dst = src[x:x+w, y:y+h].copy() #얼굴만 잘라낸 사진
+resized = cv2.resize(dst, dsize=(300,300), interpolation = cv2.INTER_AREA)
+
+
+#배경 이미지의 위치, 로고 크기만큼 컷팅
+sw = spring_warm[400:700, 400:700]
+sc = summer_cool[400:700, 400:700]
+aw = autumn_warm[400:700, 400:700]
+wc = winter_cool[400:700, 400:700]
+
+print("어떤 색깔과 비교해보시겠습니까?")
+num = int(input("1. 봄웜, 2. 여름쿨, 3. 가을웜, 4.겨울쿨 / 숫자를 입력해주세요"))
+
+if num == 1:
+    add = cv2.add(sw,resized)
+    spring_warm[400:700, 400:700] = add
+    cv2.imshow("1", spring_warm)
+
+if num == 2:
+    add = cv2.add(sc,resized)
+    summer_cool[400:700, 400:700] = add
+    cv2.imshow("1", summer_cool)
+
+if num == 3:
+    add = cv2.add(aw,resized)
+    autumn_warm[400:700, 400:700] = add
+    cv2.imshow("1", autumn_warm)
+
+if num == 4:
+    add = cv2.add(wc,resized)
+    winter_cool[400:700, 400:700] = add
+    cv2.imshow("1", winter_cool)
+
+
+#cap.release()
+#cv2.distroyAllWindows()
