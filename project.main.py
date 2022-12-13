8878bd11c3c8b917e24859b0df1ff8119d5a9f93
jared030923 <67725001+jared030923@users.noreply.github.com>
2022-12-13 12:15:17 +0900

    Add files via upload
    얼굴 캡쳐 & 자르기

--- project.main.py
+++ project.main.py
@@ -4,6 +4,13 @@ import numpy as np
 cat_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 
+spring_warm = cv2.imread('./spring_warm.jpg')
+summer_cool = cv2.imread('./summer_cool.jpg')
+autumn_warm = cv2.imread('./autumn_warm.jpg')
+winter_cool = cv2.imread('./winter_cool.jpg')
+
+x=y=w=h=0
+
 while (True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -16,7 +23,16 @@ while (True):
     cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
+    key = cv2.waitKey(30)
+    if key == 26 : #ctrl + z
+        print("captured")
+        cv2.imwrite("capture.jpg", img)
+        break
 
+src = cv2.imread("capture.jpg", cv2.IMREAD_COLOR)
+dst = src[x:x+w, y:y+h].copy() #얼굴만 잘라낸 사진 
 
+cv2.imshow("dst", dst)
 cap.release()
 cv2.distroyAllWindows()
